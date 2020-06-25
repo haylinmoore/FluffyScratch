310865182023a3511b0943127e231301b67ec037
Hampton Moore <me@hampton.pw>
2020-06-25 14:52:35 -0400

    Fix issue with name __proto__

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const axios = require('axios');
 
 var reqsPerSecond = 0;
 var queue = [];
-var notifications = {};
+var notifications = Object.create(null);
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
