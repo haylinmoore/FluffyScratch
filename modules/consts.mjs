d3663d20267affa471746775f6bbcbe95b845b81
Hampton Moore <me@hampton.pw>
2020-08-01 10:04:24 -0400

    Minorly change consts

--- modules/consts.mjs
+++ modules/consts.mjs
@@ -1,9 +1,9 @@
 const QUEUE_ITEMS_PER_SECOND = 10; // Make 10 requests a second
-const EHHH_ITEMS_PER_SECOND = 8;
+const EHHH_ITEMS_PER_SECOND = QUEUE_ITEMS_PER_SECOND - 2;
 const AUTH_CLOUD_PROJECT = 413751680; // The scratch project that handles OAuth
 const MINUTE = 1000 * 60;
 const EMPHERAL_DATA_SAVE = MINUTE; // Save Empheral data every 60s
-const ONLINE_CUTOFF_TIME = MINUTE; // Users are online if they pinged within a minute
+const ONLINE_CUTOFF_TIME = MINUTE * 2; // Users are online if they pinged within a two minutes
 const GET_USER_IDS = MINUTE * 2;
 
 export {
