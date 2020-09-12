92a9c8aae68ee4a3bc380bc1334355d6c1a7b57e
Hampton Moore <me@hampton.pw>
2020-09-12 17:48:26 -0400

    Use awaits u dumb boy

--- modules/getUserID.mjs
+++ modules/getUserID.mjs
@@ -1,51 +1,51 @@
 import { User } from "../modules/db.mjs";
 import fetch from "node-fetch";
-import queue from "./queue.mjs"
+import queue from "./queue.mjs";
 
 export default function getUserID(username) {
-    return new Promise((resolve, reject) => {
+  return new Promise(async (resolve, reject) => {
+    // First lets see if we have the userID in our database
+    let [user, created] = await User.findOrCreate({
+      where: { username: username },
+      defaults: { username: username },
+    });
 
-        // First lets see if we have the userID in our database
-        User.findOrCreate({
-            where: { username: username },
-            defaults: { username: username },
-        }).then(([user, created]) => {
-            // If we have it on file lets resolve the promise
-            if (user.id > -1) {
-                resolve(user.id);
-            }
+    // If we have it on file lets resolve the promise
+    if (user.id > -1) {
+      resolve(user.id);
+    }
 
-            fetch(
-                `https://scratchdb.lefty.one/v2/user/info/${username}`
-            )
-                .then((response) => response.json())
-                .then((data) => {
-                    // lets see if Lefty had it, if it not or if it is null
-                    if (data.error === "notfound" || data.id == null) {
-                        queue
-                            .add(
-                                queue.TYPES.GetUserProfile,
-                                {
-                                    username: user.get("username"),
-                                },
-                                queue.queues.asap
-                            )
-                            .then((profile) => {
-                                if (profile.code === "NotFound") {
-                                    user.destroy();
-                                    reject();
-                                } else {
-                                    user.set("id", profile.id);
-                                    user.save();
-                                    resolve(profile.id);
-                                }
-                            });
-                    } else {
-                        user.set("id", data.id);
-                        user.save();
-                        resolve(data.id);
-                    }
-                })
-        });
-    })
+    try {
+      let leftyResponse = await fetch(
+        `https://scratchdb.lefty.one/v2/user/info/${username}`
+      );
+
+      let leftyData = await leftyResponse.json();
+
+      if (leftyData.error === "notfound" || leftyData.id == null) {
+        throw "";
+      }
+
+      user.set("id", data.id);
+      user.save();
+      resolve(data.id);
+    } catch (err) {
+      let profile = await queue.add(
+        queue.TYPES.GetUserProfile,
+        {
+          username: user.get("username"),
+        },
+        queue.queues.asap
+      );
+
+      if (profile.code === "NotFound") {
+        user.destroy();
+        resolve("default");
+      } else {
+        user.set("id", profile.id);
+        user.save();
+        resolve(profile.id);
+      }
+    }
+  });
 }
--- routes/profilepicture.mjs
+++ routes/profilepicture.mjs
@@ -1,30 +1,22 @@
 import express from "express";
-import { User } from "../modules/db.mjs";
-import fetch from "node-fetch";
 import { isValidName } from "../modules/funcs.mjs";
 import getUserID from "../modules/getUserID.mjs";
 
 var router = express.Router();
 
-router.get("/v1/:username", (req, res) => {
+router.get("/v1/:username", async (req, res) => {
+  if (!isValidName(req.params.username)) {
+    res.json({
+      error: "The username supplied is not a valid scratch username",
+    });
+    return;
+  }
 
-	if (!isValidName(req.params.username)) {
-		res.json({ error: "The username supplied is not a valid scratch username" })
-		return;
-	}
-
-	getUserID(req.params.username).then((data) => {
-		res.redirect(
-			302,
-			`https://cdn2.scratch.mit.edu/get_image/user/${data}_60x60.png`
-		);
-	}).catch(() => {
-		res.redirect(
-			302,
-			`https://cdn2.scratch.mit.edu/get_image/user/default_60x60.png`
-		);
-	})
-
+  const id = await getUserID(req.params.username);
+  res.redirect(
+    302,
+    `https://cdn2.scratch.mit.edu/get_image/user/${id}_60x60.png`
+  );
 });
 
 export default router;
