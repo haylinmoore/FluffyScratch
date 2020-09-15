ae75d997da1fbcf56c3bc895e804dc1584748645
Hampton Moore <me@hampton.pw>
2020-09-15 07:23:31 -0400

    Move getUserID to full async and fix issue where Leftydata never used

--- modules/getUserID.mjs
+++ modules/getUserID.mjs
@@ -2,50 +2,48 @@ import { User } from "../modules/db.mjs";
 import fetch from "node-fetch";
 import queue from "./queue.mjs";
 
-export default function getUserID(username) {
-    return new Promise(async (resolve, reject) => {
-        // First lets see if we have the userID in our database
-        let [user, created] = await User.findOrCreate({
-            where: { username: username },
-            defaults: { username: username },
-        });
-
-        // If we have it on file lets resolve the promise
-        if (user.id > -1) {
-            resolve(user.id);
-        }
-
-        try {
-            let leftyResponse = await fetch(
-                `https://scratchdb.lefty.one/v2/user/info/${username}`
-            );
-
-            let leftyData = await leftyResponse.json();
-
-            if (leftyData.error === "notfound" || leftyData.id == null) {
-                jumpToTheCatchStatement()
-            }
-
-            user.set("id", data.id);
-            user.save();
-            resolve(data.id);
-        } catch (err) {
-            let profile = await queue.add(
-                queue.TYPES.GetUserProfile,
-                {
-                    username: user.get("username"),
-                },
-                queue.queues.asap
-            );
-
-            if (profile.code === "NotFound") {
-                user.destroy();
-                resolve("default");
-            } else {
-                user.set("id", profile.id);
-                user.save();
-                resolve(profile.id);
-            }
-        }
+export default async function getUserID(username) {
+    // First lets see if we have the userID in our database
+    let [user, created] = await User.findOrCreate({
+        where: { username: username },
+        defaults: { username: username },
     });
+
+    // If we have it on file lets resolve the promise
+    if (user.id > -1) {
+        return user.id;
+    }
+
+    try {
+        let leftyResponse = await fetch(
+            `https://scratchdb.lefty.one/v2/user/info/${username}`
+        );
+
+        let leftyData = await leftyResponse.json();
+
+        if (leftyData.error === "notfound" || leftyData.id == null) {
+            jumpToTheCatchStatement()
+        }
+
+        user.set("id", leftyData.id);
+        user.save();
+        return leftyData.id;
+    } catch (err) {
+        let profile = await queue.add(
+            queue.TYPES.GetUserProfile,
+            {
+                username: user.get("username"),
+            },
+            queue.queues.asap
+        );
+
+        if (profile.code === "NotFound") {
+            user.destroy();
+            return 0;
+        } else {
+            user.set("id", profile.id);
+            user.save();
+            return profile.id;
+        }
+    }
 }
