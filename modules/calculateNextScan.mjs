687a29377351abb48f142c5c59fda1557d917331
Hampton Moore <me@hampton.pw>
2020-09-15 21:50:32 -0400

    Move next scan calculation to await and fix NaN bug

--- modules/calculateNextScan.mjs
+++ modules/calculateNextScan.mjs
@@ -1,28 +1,27 @@
 import { User } from "./db.mjs";
 import { MAX_SCAN_TIMEOUT } from "./consts.mjs";
 import getProfileCommentStats from "./getProfileCommentStats.mjs";
-export default function calculateNextScan(username) {
-    new Promise((resolve, reject) => {
-        getProfileCommentStats(username).then((stats) => {
-            const date = new Date().valueOf();
-            let nextScrape = date + stats.milisecondsPerComment * 20;
-            let longest = date + MAX_SCAN_TIMEOUT;
-            if (stats.milisecondsPerComment == null || nextScrape > longest) {
-                nextScrape = longest;
-            }
-            return User.update(
-                {
-                    lastScrape: date,
-                    nextScrape: nextScrape,
-                    fullScanned: true,
-                    scanning: 0,
-                },
-                {
-                    where: {
-                        username: username,
-                    },
-                }
-            );
-        });
-    });
+export default async function calculateNextScan(username) {
+    const stats = await getProfileCommentStats(username);
+    const date = new Date().valueOf();
+    let nextScrape = date + stats.milisecondsPerComment * 20;
+    let longest = date + MAX_SCAN_TIMEOUT;
+    if (stats.milisecondsPerComment == null || nextScrape > longest || isNaN(nextScrape)) {
+        nextScrape = longest;
+    }
+    await User.update(
+        {
+            lastScrape: date,
+            nextScrape: nextScrape,
+            fullScanned: true,
+            scanning: 0,
+        },
+        {
+            where: {
+                username: username,
+            },
+        }
+    );
+
+    return;
 }
