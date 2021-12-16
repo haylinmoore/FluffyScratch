2cce17396dfd6b950b4107dcd11714eba043296f
Hampton Moore <me@hampton.pw>
2021-12-16 18:45:25 -0500

    Cryptographic secure & remove v1

--- routes/auth/getKeys/v1/[username].mjs
+++ routes/auth/getKeys/v1/[username].mjs
@@ -1,41 +1,4 @@
-import isValidName from "../../../../modules/isValidName.mjs";
-import fs from "fs";
-import empheralData from "../../../../modules/empheralData.mjs";
-
 export default async function authGetKeysv1Username(req, res) {
-    if (!isValidName(req.params.username)) {
-        res.json({ error: "The username supplied is not a valid scratch username" })
-        return;
-    }
-
-    if (!req.query.redirect) {
-        req.query.redirect = "Zmx1ZmZ5c2NyYXRjaC5oYW1wdG9uLnB3L2F1dGgvbm9SZWY"; // If no redirect send them to fluffyscratch.hampton.pw/auth/noRef
-    }
-
-    const pageData = {
-        username: req.params.username.toLowerCase(),
-        publicCode: Math.round(Math.random() * 100000).toString(), // Turns Math.random into a relativly small number
-        privateCode: (Math.random() * 100000000000000000).toString(), // Magic number that makes a Math.random an Integer
-        redirectLocation: Buffer.from(req.query.redirect, "base64").toString(
-            "utf-8"
-        ),
-    };
-
-    fs.readFile("./pages/auth.html", "utf8", function (err, authPageHTML) {
-        if (err) throw err;
-        for (let item in pageData) {
-            authPageHTML = authPageHTML.replace(
-                new RegExp(`{{${item}}}`, "g"),
-                pageData[item]
-            );
-        }
-
-        authPageHTML = authPageHTML.replace(
-            new RegExp(`{{redirectLocationB64}}`, "g"),
-            req.query.redirect
-        );
-
-        empheralData.auth[pageData.username] = pageData;
-        res.send(authPageHTML);
-    });
+    res.json({ error: "This endpoint has been removed." })
+    return;
 };
--- routes/auth/getKeys/v2.mjs
+++ routes/auth/getKeys/v2.mjs
@@ -1,15 +1,20 @@
 import isValidName from "../../../modules/isValidName.mjs";
 import fs from "fs";
-import {Auth} from "../../../modules/db.mjs";
+import crypto from "crypto";
+import { Auth } from "../../../modules/db.mjs";
+
+const randomKey = async (length) => {
+    const buffer = await crypto.randomBytes(length);
+    return buffer
+};
 
 export default async function authGetKeysv2(req, res) {
     if (!req.query.redirect) {
         req.query.redirect = "Zmx1ZmZ5c2NyYXRjaC5oYW1wdG9uLnB3L2F1dGgvbm9SZWY"; // If no redirect send them to fluffyscratch.hampton.pw/auth/noRef
     }
-
     const pageData = {
-        publicCode: Math.round(Math.random() * 100000).toString(), // Turns Math.random into a relativly small number
-        privateCode: (Math.random() * 100000000000000000).toString(), // Magic number that makes a Math.random an Integer
+        publicCode: (await randomKey(3)).reduce((a, b) => (a + 1) * (b + 1)).toString(), // Turns Math.random into a relativly small number
+        privateCode: (await randomKey(16)).toString("hex"), // Magic number that makes a Math.random an Integer
         redirectLocation: Buffer.from(req.query.redirect, "base64").toString(
             "utf-8"
         ),
