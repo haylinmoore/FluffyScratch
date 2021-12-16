018c022db50af3b20bc7ce9fb2f9aa51a7e02382
Hampton Moore <me@hampton.pw>
2021-12-16 18:49:32 -0500

    Whoops

--- routes/auth/getKeys/v2.mjs
+++ routes/auth/getKeys/v2.mjs
@@ -13,8 +13,8 @@ export default async function authGetKeysv2(req, res) {
         req.query.redirect = "Zmx1ZmZ5c2NyYXRjaC5oYW1wdG9uLnB3L2F1dGgvbm9SZWY"; // If no redirect send them to fluffyscratch.hampton.pw/auth/noRef
     }
     const pageData = {
-        publicCode: (await randomKey(3)).reduce((a, b) => (a + 1) * (b + 1)).toString(), // Turns Math.random into a relativly small number
-        privateCode: (await randomKey(16)).toString("hex"), // Magic number that makes a Math.random an Integer
+        publicCode: (await randomKey(3)).reduce((a, b) => (a + 1) * (b + 1)).toString(),
+        privateCode: ((await randomKey(12)).reduce((a, b) => (a + 1) * (b + 1)) % 100000000000000000).toString(),
         redirectLocation: Buffer.from(req.query.redirect, "base64").toString(
             "utf-8"
         ),
