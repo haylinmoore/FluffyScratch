bd13cfb1cf9ca7ed9fdff6fe85ba30101ce7a7cf
Hampton Moore <me@hampton.pw>
2021-12-17 06:53:10 -0500

    Increase entropy count

--- modules/consts.mjs
+++ modules/consts.mjs
@@ -3,7 +3,7 @@ dotenv.config();
 
 const QUEUE_ITEMS_PER_SECOND = 10; // Make 10 requests a second unless im testing locally
 const EHHH_ITEMS_PER_SECOND = QUEUE_ITEMS_PER_SECOND - 2;
-const AUTH_CLOUD_PROJECT = 514649494; // The scratch project that handles OAuth
+const AUTH_CLOUD_PROJECT = 413751680; // The scratch project that handles OAuth
 const MINUTE = 1000 * 60;
 const EMPHERAL_DATA_SAVE = MINUTE; // Save Empheral data every 60s
 const ONLINE_CUTOFF_TIME = MINUTE * 2; // Users are online if they pinged within a two minutes
@@ -16,15 +16,15 @@ const MAX_COMMENT_PAGE = 67;
 const PORT = parseInt(process.env.PORT, 10) || 3000;
 
 export {
-	QUEUE_ITEMS_PER_SECOND,
-	AUTH_CLOUD_PROJECT,
-	EMPHERAL_DATA_SAVE,
-	ONLINE_CUTOFF_TIME,
-	EHHH_ITEMS_PER_SECOND,
-	GET_USER_IDS,
-	MINUTE,
-	SCAN_PROFILES,
-	MAX_SCAN_TIMEOUT,
-	MAX_COMMENT_PAGE,
-	PORT
+    QUEUE_ITEMS_PER_SECOND,
+    AUTH_CLOUD_PROJECT,
+    EMPHERAL_DATA_SAVE,
+    ONLINE_CUTOFF_TIME,
+    EHHH_ITEMS_PER_SECOND,
+    GET_USER_IDS,
+    MINUTE,
+    SCAN_PROFILES,
+    MAX_SCAN_TIMEOUT,
+    MAX_COMMENT_PAGE,
+    PORT
 };
